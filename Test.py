abb833e806040182323fb09d5f6a10e298236e79
Ugo Louche <louche.ugo@ironova.com>
2018-03-06 03:44:52 -0600

    Files sorting

--- Test.py
+++ Test.py
@@ -1,38 +1,53 @@
 import os
+import operator
 
 import numpy as np
 
 from difflib import SequenceMatcher
 
 img_path = "/media/BunNet/pictures/"
+verbose=False
 
 img_names = sorted(os.listdir(img_path))
 
-thres = 0.8
+#Replace digits with 'X' becasue gitis are part of the pattern but their values are irrelevent.
+#Also remove file extensions
+#Original filename can be found from the position of the item in the list (this is a ugly hack... and rely on sorted listdir)
+img_names_filt = []
+for idImg in range(len(img_names)):
+    img_names_filt.append(''.join(i if not i.isdigit() else 'X' for i in img_names[idImg])\
+                              .split('.')[0])
+
+thres = 0.7 #Set empirically
 di_class = {}
 #For each images
-for idImg, img in enumerate(img_names):
+for idImg, img in enumerate(img_names_filt):
     #Rank Dictionnary keys
     di_keyRank = {}
     for key in di_class:
-        avg_score = []
-        for i in di_class[key]:
-            avg_score = SequenceMatcher(None, i, img).ratio()
-        di_keyRank[key] = np.mean(avg_score)
+        #Speedup: skip key with low match ratio as they won't probably met the required avg ratio.
+        if SequenceMatcher(None, key, img).ratio() < 0.5*thres:
+            di_keyRank[key] = SequenceMatcher(None, key, img).ratio()
+            if verbose: print('Early skip, key:{}, img: {}, ratio: {}'.format(key, img, di_keyRank[key]))
+        else:
+            avg_score = []
+            for i in di_class[key]:
+                avg_score = SequenceMatcher(None, i[0], img).ratio()
+            di_keyRank[key] = np.mean(avg_score)
+            
     #Store img under the key with highest avg_score, if it's above ratio
     if len(di_keyRank) != 0:
-        maxKey = max(di_keyRank)
+        maxKey = max(di_keyRank.items(), key=operator.itemgetter(1))[0]
         if di_keyRank[maxKey] > thres:
-            di_class[maxKey].append(img)
-            print('img: {} stored under key {} with avg ratio of {}'\
-                  .format(img, maxKey, di_keyRank[maxKey]))
+            di_class[maxKey].append((img, idImg))
+            if verbose: print('img: {} stored under key {} with avg ratio of {}'\
+                              .format(img, maxKey, di_keyRank[maxKey]))
             continue
-    
     # If none exist, create a new one.
-    di_class[img] = [img]
+    di_class[img] = [(img, idImg)]
     print('img {} stored under a new key'.format(img))
     if idImg % (int(len(img_names)/100)) == 0:
-        print('Done {} img over {} ({}%)'.format())
+        print('Done {} img over {} ({:.1f}%)'.format(idImg, len(img_names), 100*idImg/len(img_names)))
 
 #Gather some stats
 keySize = [len(di_class[key]) for key in di_class]
